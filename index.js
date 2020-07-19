ebafc231d5015573edc201862482856447ae01b3
Bill Barsch <billbarsch@gmail.com>
2020-07-19 20:16:40 -0300

    fix.php

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 var express = require("express");
+var venom = require('venom-bot');
+
+var client = venom.create().then((client) => start(client));
+
+function start(client) {
+    client.onMessage((message) => {
+        if (message.body === 'Hi') {
+            client.sendText(message.from, 'Welcome Venom 🕷');
+        }
+    });
+}
+
 var app = express();
 app.listen(3333, () => {
     console.log("Server running on port 3333");
 });
 
 app.get("/message", (req, res, next) => {
-    res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
+    res.json(req);
 });
