6528e3559c2a498c9b509fad8cb9e3b172568588
Bill Barsch <billbarsch@gmail.com>
2020-07-19 20:47:43 -0300

    fix.php

--- index.js
+++ index.js
@@ -2,29 +2,24 @@ var express = require("express");
 var venom = require('venom-bot');
 
 //const client = venom.create().then((client) => start(client));
-const client = venom.create();
+venom.create().then((client) => start(client));
 
-/*
 function start(client) {
+
+    var app = express();
+    app.listen(3333, () => {
+        console.log("Server running on port 3333");
+    });
+
+    app.get("/message", async (req, res, next) => {
+        //client.sendMessageToId(req.query.number + '@c.us', req.query.message);
+        let chats = await client.getAllGroups();
+        res.json(chats);
+    });
+
     client.onMessage((message) => {
         if (message.body === 'Hi') {
             client.sendText(message.from, 'Welcome Venom 🕷');
         }
     });
 }
-*/
-
-var app = express();
-app.listen(3333, () => {
-    console.log("Server running on port 3333");
-});
-
-app.get("/message", async (req, res, next) => {
-    //client.sendMessageToId(req.query.number + '@c.us', req.query.message);
-    let chats = await client.getAllGroups();
-    res.json(chats);
-});
-
-process.on('SIGINT', function () {
-    client.close();
-});
