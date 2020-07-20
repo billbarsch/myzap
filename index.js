794ce6d8524c9426c91550417fb7d60c41fec5d8
Bill Barsch <billbarsch@gmail.com>
2020-07-20 11:52:04 -0300

    fix.php

--- index.js
+++ index.js
@@ -2,7 +2,38 @@ var express = require("express");
 var venom = require('venom-bot');
 
 //const client = venom.create().then((client) => start(client));
-venom.create(null, null, null, { browserArgs: ['--no-sandbox'] }).then((client) => start(client));
+venom.create(null, null, null, {
+    browserArgs: [
+        '--log-level=3',
+        '--no-default-browser-check',
+        '--disable-site-isolation-trials',
+        '--no-experiments',
+        '--ignore-gpu-blacklist',
+        '--ignore-certificate-errors',
+        '--ignore-certificate-errors-spki-list',
+        '--disable-gpu',
+        '--disable-extensions',
+        '--disable-default-apps',
+        '--enable-features=NetworkService',
+        '--disable-setuid-sandbox',
+        '--no-sandbox',
+        // Extras
+        '--disable-webgl',
+        '--disable-threaded-animation',
+        '--disable-threaded-scrolling',
+        '--disable-in-process-stack-traces',
+        '--disable-histogram-customizer',
+        '--disable-gl-extensions',
+        '--disable-composited-antialiasing',
+        '--disable-canvas-aa',
+        '--disable-3d-apis',
+        '--disable-accelerated-2d-canvas',
+        '--disable-accelerated-jpeg-decoding',
+        '--disable-accelerated-mjpeg-decode',
+        '--disable-app-list-dismiss-on-blur',
+        '--disable-accelerated-video-decode',
+    ]
+}).then((client) => start(client));
 
 function start(client) {
 
