fddf5a2f2cad65ad6864c13d521613cc56ed3b21
Bill Barsch <billbarsch@gmail.com>
2021-02-14 16:49:50 -0300

    limpar token na nuvem quando chamar /close

--- index.js
+++ index.js
@@ -100,6 +100,30 @@ app.post("/sendFile", async (req, res, next) => {
 });//sendFile
 
 app.get("/close", async (req, res, next) => {
+    if (Sessions.options.jsonbinio_secret_key !== undefined) {//se informou secret key pra salvar na nuvem
+        setTimeout(async () => {
+            console.log("limpando token na nuvem...");
+            //salva dados do token da sessão na nuvem
+            var data = JSON.stringify({ "nada": "nada" });
+            var config = {
+                method: 'put',
+                url: 'https://api.jsonbin.io/b/' + Sessions.options.jsonbinio_bin_id,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'secret-key': Sessions.options.jsonbinio_secret_key,
+                    'versioning': 'false'
+                },
+                data: data
+            };
+            await axios(config)
+                .then(function (response) {
+                    console.log(JSON.stringify(response.data));
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }, 2000);
+    }
     var result = await Sessions.closeSession(req.query.sessionName);
     res.json(result);
 });//close
