d513dea8523ccfbf6dd54f0436265dc9942dbf4b
Alan Martines <alancpmartines@hotmail.com>
2021-11-17 12:36:44 -0400

    Fix implementado opção para host em ssl

--- config.js
+++ config.js
@@ -8,27 +8,28 @@
 ##############################################################################*/
 
 //'use strict';
-import  dotenv  from "dotenv";
+import dotenv from "dotenv";
 import assert from "assert";
 
 dotenv.config();
 
 const {
-    PORT,
-    HOST,
-    TOKEN,
-    HTTPS,
-    ENGINE,
-    SSL_KEY_PATH,
-    SSL_CERT_PATH,
-    API_KEY,
-    AUTH_DOMAIN,
-    PROJECT_ID,
-    STORAGE_BUCKET,
-    MESSAGING_SENDER_ID,
-    APP_ID,
-    START_ALL_SESSIONS,
-    FORCE_CONNECTION_USE_HERE
+  PORT,
+  HOST,
+  TOKEN,
+  HTTPS,
+  HOST_SSL,
+  ENGINE,
+  SSL_KEY_PATH,
+  SSL_CERT_PATH,
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+  START_ALL_SESSIONS,
+  FORCE_CONNECTION_USE_HERE
 } = process.env;
 
 assert(PORT, 'PORT is required, please set the PORT variable value in the .env file');
@@ -38,23 +39,22 @@ assert(ENGINE, 'ENGINE is required, please set the ENGINE variable value in the 
 
 
 export default {
-    port: PORT,
-    host: HOST,
-    token: TOKEN,
-    https: HTTPS,
-    engine: ENGINE,
-    ssl_key_path: SSL_KEY_PATH,
-    ssl_cert_path: SSL_CERT_PATH,
-    firebaseConfig: {
-        apiKey: API_KEY,
-        authDomain: AUTH_DOMAIN,
-        projectId: PROJECT_ID,
-        storageBucket: STORAGE_BUCKET,
-        messagingSenderId: MESSAGING_SENDER_ID,
-        appId: APP_ID
-    },
-    start_all_sessions: START_ALL_SESSIONS,
-    useHere: FORCE_CONNECTION_USE_HERE,
-
-    
+  port: PORT,
+  host: HOST,
+  token: TOKEN,
+  https: HTTPS,
+  host_ssl: HOST_SSL,
+  engine: ENGINE,
+  ssl_key_path: SSL_KEY_PATH,
+  ssl_cert_path: SSL_CERT_PATH,
+  firebaseConfig: {
+    apiKey: API_KEY,
+    authDomain: AUTH_DOMAIN,
+    projectId: PROJECT_ID,
+    storageBucket: STORAGE_BUCKET,
+    messagingSenderId: MESSAGING_SENDER_ID,
+    appId: APP_ID
+  },
+  start_all_sessions: START_ALL_SESSIONS,
+  useHere: FORCE_CONNECTION_USE_HERE,
 }
