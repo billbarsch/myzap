feb58c0ae2575ec6eac12f10dfd5332a3e994f1a
Alan Martines <alancpmartines@hotmail.com>
2021-12-24 16:23:41 -0400

    Inicial Update

--- startup.js
+++ startup.js
@@ -55,7 +55,7 @@ async function startAllSessions() {
                 var options = {
                     'method': 'POST',
                     'json': true,
-                    'url': `${config.host}:${config.port}/start`,
+                    'url': `http://${config.host}:${config.port}/start`,
                     'headers': {
                         'apitoken': item.apitoken,
                         'sessionkey': item.sessionkey
