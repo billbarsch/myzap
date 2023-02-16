8b71d8a15ca3c2c0d6111d12697c1aca71f17f89
Your NameHamilton <hamilton@hts-net.com.br>
2023-02-16 04:27:48 -0300

    incluído 5 segundos de indicação de digitação antes de enviar uma mensagem

--- sessions.js
+++ sessions.js
@@ -349,6 +349,10 @@ module.exports = class Sessions {
         if (session) {
             if (session.state == "CONNECTED") {
                 await session.client.then(async client => {
+                    console.log(pegaDataHora() + 'simulando digitação');
+                    await client.startTyping(params.number + '@c.us');
+                    await sleep(5000);
+                    await client.stopTyping(params.number + '@c.us');
                     console.log(pegaDataHora() + '#### send msg =', params);
                     return await client.sendText(params.number + '@c.us', params.text);
                 });
@@ -684,4 +688,9 @@ function pegaDataHora() {
     var today = new Date();
     var date = today.getFullYear()+'-'+String(today.getMonth()+1).padStart(2, '0')+'-'+String(today.getDate()).padStart(2, '0') + ' ' + String(today.getHours()).padStart(2, '0') + ':' + String(today.getMinutes()).padStart(2, '0') + ':' + String(today.getSeconds()).padStart(2, '0') + ': ';
     return date;
+}
+
+//function to wait x ms
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
 }
